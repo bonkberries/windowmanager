f242f666c96aeec17c8ad8a40bacf86620663ed6
bonkberries <65596971+bonkberries@users.noreply.github.com>
2021-02-20 20:48:27 +0300

    Update bspwminstall.sh

--- bspwminstall.sh
+++ bspwminstall.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 echo "installing sxhkd config"
-
+sleep 1
 mkdir $HOME/.config/
 mkdir $HOME/.config/sxhkd
 install sxhkdrc $HOME/.config/sxhkd/sxhkdrc
-
+echo ""
+sleep 2
+echo ""
 echo "installing bspwm config"
-
+sleep 1
 mkdir $HOME/.config/bspwm
 install bspwmrc $HOME/.config/bspwm/bspwmrc
 chmod +x $HOME/.config/bspwm/bspwmrc
+echo ""
+sleep 2
+echo "configs are done"
